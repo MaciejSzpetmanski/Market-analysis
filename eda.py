4324de1d20f727f05df7254f297e1209a8e24c0d
kubek22 <01171264@pw.edu.pl>
2024-11-07 19:26:25 +0100

    extracting time features

--- eda.py
+++ eda.py
@@ -11,9 +11,11 @@ import numpy as np
 import pandas as pd
 
 
-def load_data(directory_name):
+def load_data(directory_name, suffix=""):
     df = None
     for file_name in os.listdir(directory_name):
+        if not file_name.endswith(suffix):
+            continue
         file_path = os.path.join(directory_name, file_name)
         if os.path.isfile(file_path) and file_path.endswith(".csv"):
             df_input = pd.read_csv(file_path)
@@ -32,7 +34,10 @@ def load_data(directory_name):
             df = pd.concat([df, df_input], ignore_index=True)
     return df
 
-df = load_data("data")
+
+# df = load_data("data")
+df = load_data("data", "_data_1d.csv") # only daily frequency ???
+
 df.shape
 df.head()
 df.columns
@@ -53,21 +58,38 @@ df.info()
 
 #%% removing duplicates
 
-df.drop(columns=['Name', 'Freq']).shape[0]
-df.drop(columns=['Name', 'Freq']).drop_duplicates().shape[0]
-
-subset = list(filter(lambda x: x not in ['Name', 'Freq'], df.columns))
+subset = list(filter(lambda x: x != 'Freq', df.columns))
 df.drop_duplicates(subset=subset, inplace=True)
-df.reset_index()
+df = df.reset_index(drop=True)
 
 #%% time handling
 
+from feature_engine.datetime import DatetimeFeatures
+
 df['Date'] = pd.to_datetime(df['Date'])
-df['Year'] = df['Date'].apply(lambda x: x.year)
-df['Month'] = df['Date'].apply(lambda x: x.month)
-df['Day'] = df['Date'].apply(lambda x: x.day)
 df['Timestamp'] = df['Date'].astype('int64') // 10**9
-# we can add more columns (Hour, Minute) if needed
+
+# saving date
+date = df['Date']
+
+dt_features = DatetimeFeatures(
+    variables=['Date'],
+    features_to_extract=[
+        'month', 'quarter', 'semester', 'year', 'week', 
+        'day_of_week', 'day_of_month', 'day_of_year', 
+        'weekend', 'month_start', 'month_end', 
+        'quarter_start', 'quarter_end', 'year_start', 
+        'year_end', 'leap_year', 'days_in_month', 
+        'hour', 'minute', 'second'
+    ]
+)
+
+df = dt_features.fit_transform(df)
+df['Date'] = date
+
+df.info()
+
+# TODO identify useless columns
 
 #%% name encoding (one-hot)
 
@@ -89,13 +111,16 @@ split_date = last_date - pd.DateOffset(years=1)
 df_train = df[df['Date'] <= split_date]
 df_train.shape
 
-df_val, df_test = train_test_split(df[df['Date'] > split_date], test_size=0.6, random_state=42, stratify=df[df['Date'] > split_date]['Year'])
+# TODO stratify by day-month-year
+df_val, df_test = train_test_split(df[df['Date'] > split_date], test_size=0.6, random_state=42, stratify=df[df['Date'] > split_date]['Date_year'])
 
-print(f'train: {len(df_train)}')
-print(f'val: {len(df_val)}')
-print(f'test: {len(df_test)}')
+# OPTIONAL hide warnings
+for data in [df_train, df_val, df_test]:
+    data.drop(columns=['Date'], inplace=True)
 
-df.drop(columns=['Date'], inplace=True)
+print(f'train: {df_train.shape}')
+print(f'val: {df_val.shape}')
+print(f'test: {df_test.shape}')
 
 #%% correlation
 
